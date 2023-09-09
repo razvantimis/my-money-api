aadff62405fae5110b231ecde0910f80ff7c593d
Razvan Timis <r@timis.dev>
2023-09-09 15:08:32 +0300

    fix mapping

--- src/features/dividends/dividends.ts
+++ src/features/dividends/dividends.ts
@@ -1,10 +1,10 @@
 import type { fetchFlexStatmentData } from "../interactiveBrokersApi";
-import { parse, isEqual } from "date-fns";
+import { parse, isEqual, format } from "date-fns";
 import { zonedTimeToUtc } from "date-fns-tz";
 import parseDividends from "./parseDividends";
 
 type MyDividend = {
-  date: Date;
+  uuid: string;
   exDate: Date;
   payDate: Date;
   symbol: string;
@@ -26,9 +26,16 @@ type MyDividend = {
   netAmount: number;
 };
 
+function getUUID(dividend: Pick<MyDividend, "payDate" | "symbol" | "exchangeCode" | "account">) {
+  return `${format(dividend.payDate, 'yyyy-MM-dd')}#${dividend.symbol}#${dividend.exchangeCode}#${dividend.account}`;
+}
+
 type ExternalApi = {
   fetchStatmentData: typeof fetchFlexStatmentData;
 };
+const exchangeCode = "US";
+const account = "INTERACTIVE_BROKERS";
+const currency = "USD";
 
 export const getDividends =
   ({ fetchStatmentData }: ExternalApi) =>
@@ -37,26 +44,32 @@ export const getDividends =
     const dividends = parseDividends(xmlData);
 
     const resultDividends: MyDividend[] = [];
+ 
     for (const dividend of dividends) {
+      // avoid duplicate dividends
       if (dividend["@date"] !== dividend["@payDate"]) {
         continue;
       }
       const exDate = parse(dividend["@exDate"], "yyyyMMdd", new Date());
       const payDate = parse(dividend["@payDate"], "yyyyMMdd", new Date());
-      const date = parse(dividend["@date"], "yyyyMMdd", new Date());
       const myDividend: MyDividend = {
-        date: zonedTimeToUtc(date, "UTC"),
+        uuid: getUUID({
+          payDate,
+          symbol: dividend["@symbol"],
+          exchangeCode,
+          account,
+        }),
         exDate: zonedTimeToUtc(exDate, "UTC"),
         payDate: zonedTimeToUtc(payDate, "UTC"),
         symbol: dividend["@symbol"],
         netAmount: Math.abs(parseFloat(dividend["@netAmount"])),
         grossAmount: Math.abs(parseFloat(dividend["@grossAmount"])),
         fee: parseFloat(dividend["@fee"]),
-        currency: "USD",
+        currency,
         quantity: parseFloat(dividend["@quantity"]),
         grossRate: parseFloat(dividend["@grossRate"]),
-        account: "INTERACTIVE_BROKERS",
-        exchangeCode: "US",
+        account,
+        exchangeCode,
         tax: Math.abs(parseFloat(dividend["@tax"])),
         description: dividend["@description"],
       };
